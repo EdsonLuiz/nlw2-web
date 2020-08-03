5ddffd94aadfe47d3ff57da8e301516870849c48
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-03 12:21:55 -0300

    configuraões globais

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './assets/styles/global.css'
+
+const App:React.FC = () => {
+  return (
+    <>
+      <h1>Hello from nlw2</h1>
+    </>
+  );
+}
+
+export default App;
