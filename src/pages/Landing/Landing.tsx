3b647ecb3718fbd9ff6fbefd4362b1a88f880bcd
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-03 16:12:22 -0300

    refactor: change from tag a to Link component

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 import './styles.css'
 
@@ -22,14 +23,14 @@ const Landing:React.FC = () => {
           alt="Plataforma de estudos" className="hero-image"/>
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to={'/study'} className="study">
             <img src={studyIcon} alt="Estudar"/>
             Estudar
-          </a>
-          <a href="" className="give-classes">
+          </Link>
+          <Link to={'/give-classes'} className="give-classes">
             <img src={giveClassesIcon} alt="Estudar"/>
             Dar aulas
-          </a>
+          </Link>
         </div>
 
         <span className="total-connections">
