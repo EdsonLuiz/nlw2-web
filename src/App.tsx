26de43040af4d6d2c92e2eec37c13af24376063e
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-03 15:04:32 -0300

    feat: add Landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './assets/styles/global.css'
+import {Landing} from './pages/Landing';
 
 const App:React.FC = () => {
   return (
     <>
-      <h1>Hello from nlw2</h1>
+      <Landing />
     </>
   );
 }
