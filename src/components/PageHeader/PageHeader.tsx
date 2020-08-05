2d21724f4f3fcbf722c46f6571bbc3d5ff917dcb
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-05 10:05:50 -0300

    feat: add input and style to teacher form page

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -8,10 +8,12 @@ import logoImg from '../../assets/images/logo.svg'
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
 const PageHeader:React.FC<PageHeaderProps> = (props) => {
-  const {title, children} = props
+
+  const {title, children, description} = props
   return (
       <header className="page-header">
         <div className="top-bar-container">
@@ -23,6 +25,7 @@ const PageHeader:React.FC<PageHeaderProps> = (props) => {
 
         <div className="header-content">
           <strong>{title}</strong>
+          {description && <p>{description}</p>}
           {children}
         </div>
 
--- src/pages/TeacherForm/TeacherForm.tsx
+++ src/pages/TeacherForm/TeacherForm.tsx
@@ -1,12 +1,36 @@
 import React from 'react'
 import {PageHeader} from '../../components/PageHeader'
 
+import './styles.css'
+
 const TeacherForm:React.FC = () => {
   const title = "Que incrivel que você quer dar aulas."
   return (
-    <>
-      <PageHeader title={title} />
-    </>
+    <div id="page-teacher-form" className="container">
+      <PageHeader 
+        description="O primeiro passo é preencher esse formulário de inscrição."
+        title={title} />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+          <div className="input-block">
+            <label htmlFor="name">Nome Completo</label>
+            <input type="text" id="name"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="avatar">Avatar</label>
+            <input type="text" id="avatar"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="whatsapp">Whatsapp</label>
+            <input type="text" id="whatsapp"/>
+          </div>
+        </fieldset>
+      </main>
+    </div>
   )
 }
 
